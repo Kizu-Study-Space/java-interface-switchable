53b7c9d5f9de6b6354dedb745d475576cd1a5dbb
Zora Fuchs <13620981+zorafuchs@users.noreply.github.com>
2023-10-18 11:14:29 +0000

    Add switchable interface with javadoc

---
+++ src/main/java/switchables/Switchable.java
@@ -0,0 +1,31 @@
+package switchable;
+
+/**
+* Interface for implementing objects that can have a running state. Switch them off and on.
+*/
+public interface Switchable {
+
+  /**
+   * Switches an object on into a running state.
+   */
+  void switchOn();
+
+  /**
+   * Switches an object off.
+   */
+  void switchOff();
+
+  /**
+   * Tells if the object is switched on.
+   * 
+   * @return boolean
+   */
+  boolean isSwitchedOn();
+
+  /**
+   * Tells if the object is switched off.
+   * 
+   * @return boolean
+   */
+  boolean isSwitchedOff();
+}
