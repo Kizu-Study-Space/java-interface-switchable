de54eb8fefbf2f2eedeafb5ecc3fbc4408a7dfb3
zorafuchs <13620981+zorafuchs@users.noreply.github.com>
2023-10-23 17:03:36 +0200

    Implement and test Motor class

---
+++ src/main/java/ch/hslu/oop/switchables/Motor.java
@@ -0,0 +1,50 @@
+package ch.hslu.oop.switchables;
+
+public class Motor implements Switchable {
+
+    int currentRpm;
+    int idleGas;
+    int maxRpm;
+
+    public Motor(final int idleGas, final int maxRpm) {
+        this.currentRpm = 0;
+        this.idleGas = idleGas;
+        this.maxRpm = maxRpm;
+    }
+
+    public Motor () {
+        this(1000, 5000);
+    }
+
+    @Override
+    public void switchOn() {
+        this.currentRpm = this.idleGas;
+    }
+
+    @Override
+    public void switchOff() {
+        this.currentRpm = 0;
+    }
+
+    @Override
+    public boolean isSwitchedOn() {
+        return this.currentRpm > 0;
+    }
+
+    @Override
+    public boolean isSwitchedOff() {
+        return !this.isSwitchedOn();
+    }
+
+    public int getCurrentRpm() {
+        return this.currentRpm;
+    }
+
+    public int getIdleGas() {
+        return idleGas;
+    }
+
+    public int getMaxRpm() {
+        return maxRpm;
+    }
+}
---
+++ src/test/java/ch/hslu/oop/switchables/MotorTest.java
@@ -0,0 +1,55 @@
+package ch.hslu.oop.switchables;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MotorTest {
+
+    @Test
+    public void testGetIdleGas() {
+        Motor motor = new Motor(1000, 5000);
+        assertEquals(1000, motor.getIdleGas());
+    }
+
+    @Test
+    public void testGetMaxRpm() {
+        Motor motor = new Motor(1000, 5000);
+        assertEquals(5000, motor.getMaxRpm());
+    }
+
+    @Test
+    public void testGetCurrentRpm() {
+        Motor motor = new Motor(1000, 5000);
+        assertEquals(0, motor.getCurrentRpm());
+    }
+
+    @Test
+    public void testSwitchOn() {
+        Motor motor = new Motor(1000, 5000);
+        motor.switchOn();
+        assertEquals(1000, motor.getCurrentRpm());
+    }
+
+    @Test
+    public void testSwitchOff() {
+        Motor motor = new Motor(1000, 5000);
+        motor.switchOn();
+        motor.switchOff();
+        assertEquals(0, motor.getCurrentRpm());
+    }
+
+    @Test
+    public void testSwitchedOn() {
+        Motor motor = new Motor(1000, 5000);
+        motor.switchOn();
+        assertTrue(motor.isSwitchedOn());
+    }
+
+    @Test
+    public void testSwitchedOff() {
+        Motor motor = new Motor(1000, 5000);
+        motor.switchOn();
+        motor.switchOff();
+        assertTrue(motor.isSwitchedOff());
+    }
+}
