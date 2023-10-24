eaa8b77946bcd16e8707547edf0e1ed0de000118
zorafuchs <13620981+zorafuchs@users.noreply.github.com>
2023-10-24 16:56:36 +0200

    Fix vehicle test and class

--- src/test/java/ch/hslu/oop/switchables/VehicleTest.java
+++ src/test/java/ch/hslu/oop/switchables/VehicleTest.java
@@ -1,53 +1,60 @@
 package ch.hslu.oop.switchables;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class VehicleTest {
 
+    @Test
     public void testGetMotor() {
         Motor motor = new Motor(1000, 5000);
         Vehicle vehicle = new Vehicle(motor, new Wheel(), new Wheel(), new Wheel(), new Wheel(), new Light(), new Light());
         assertEquals(motor, vehicle.getMotor());
     }
 
+    @Test
     public void testGetFrontRightWheel() {
         Wheel wheel = new Wheel();
         Vehicle vehicle = new Vehicle(new Motor(), wheel, new Wheel(), new Wheel(), new Wheel(), new Light(), new Light());
         assertEquals(wheel, vehicle.getFrontRightWheel());
     }
 
+    @Test
     public void testGetFrontLeftWheel() {
         Wheel wheel = new Wheel();
         Vehicle vehicle = new Vehicle(new Motor(), new Wheel(), wheel, new Wheel(), new Wheel(), new Light(), new Light());
         assertEquals(wheel, vehicle.getFrontLeftWheel());
     }
 
+    @Test
     public void testGetBackRightWheel() {
         Wheel wheel = new Wheel();
         Vehicle vehicle = new Vehicle(new Motor(), new Wheel(), new Wheel(), wheel, new Wheel(), new Light(), new Light());
         assertEquals(wheel, vehicle.getBackRightWheel());
     }
 
+    @Test
     public void testGetBackLeftWheel() {
         Wheel wheel = new Wheel();
         Vehicle vehicle = new Vehicle(new Motor(), new Wheel(), new Wheel(), new Wheel(), wheel, new Light(), new Light());
         assertEquals(wheel, vehicle.getBackLeftWheel());
     }
 
+    @Test
     public void testGetLeftLight() {
         Light light = new Light();
         Vehicle vehicle = new Vehicle(new Motor(), new Wheel(), new Wheel(), new Wheel(), new Wheel(), light, new Light());
         assertEquals(light, vehicle.getLeftLight());
     }
 
+    @Test
     public void testGetRightLight() {
         Light light = new Light();
         Vehicle vehicle = new Vehicle(new Motor(), new Wheel(), new Wheel(), new Wheel(), new Wheel(), new Light(), light);
         assertEquals(light, vehicle.getRightLight());
     }
 
+    @Test
     public void testSwitchOn() {
         Motor motor = new Motor(1000, 5000);
         Vehicle vehicle = new Vehicle(motor, new Wheel(), new Wheel(), new Wheel(), new Wheel(), new Light(), new Light());
@@ -58,6 +65,7 @@ public class VehicleTest {
         assertTrue(vehicle.isSwitchedOn());
     }
 
+    @Test
     public void testIsSwitchedOff() {
         Vehicle vehicle = new Vehicle(new Motor(), new Wheel(), new Wheel(), new Wheel(), new Wheel(), new Light(), new Light());
         assertTrue(vehicle.isSwitchedOff());
@@ -65,6 +73,7 @@ public class VehicleTest {
         assertFalse(vehicle.isSwitchedOff());
     }
 
+    @Test
     public void testIsSwitchedOn() {
         Vehicle vehicle = new Vehicle(new Motor(), new Wheel(), new Wheel(), new Wheel(), new Wheel(), new Light(), new Light());
         assertFalse(vehicle.isSwitchedOn());
@@ -72,6 +81,7 @@ public class VehicleTest {
         assertTrue(vehicle.isSwitchedOn());
     }
 
+    @Test
     public void testSwitchOff () {
         Motor motor = new Motor(1000, 5000);
         Vehicle vehicle = new Vehicle(motor, new Wheel(), new Wheel(), new Wheel(), new Wheel(), new Light(), new Light());
